3cccf81a71191f99847275b5f75238db09e77238
Patrick Miner <patrickminer318@gmail.com>
2018-02-14 23:04:09 -0800

    Added ReactRouter. Created first route PostIndex which will list all blog posts

---
+++ src/components/post_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostIndex extends Component {
+  render() {
+    return (
+      <div>I've made a post index</div>
+    );
+  }
+}
+
+export default PostIndex;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostIndex from './components/post_index'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostIndex} />
+        {/* <Route path="/posts/:id" component={PostDetail} /> 
+        <Route path="/posts/new" component={PostNew} /> */}
+      </div>
+    </BrowserRouter>
   </Provider>
+
   , document.querySelector('.container'));
