c9d043a3659a2413cb0387cb9292b72e2e1978de
Patrick Miner <patrickminer318@gmail.com>
2018-02-16 00:14:09 -0800

    Modularized form inputs and added all 3 inputs required for a post

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import {Field, reduxForm } from 'redux-form';
 
 class NewPost extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
-        <input 
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+        className="form-control" 
+        type='text'
           {...field.input}
         />
       </div>
@@ -15,10 +18,22 @@ class NewPost extends Component {
   render() {
     return (
       <form>
-        <Field 
+        <Field
+          label="Title of Post"
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
         />
+        <Field 
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field 
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+
       </form>
     );
   }
